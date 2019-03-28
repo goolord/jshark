dbe3beeaa5a6eaf6790d16959225cdc416bd5809
chessai <chessai1996@gmail.com>
2019-03-28 07:18:41 -0400

    make Action a Functor and rewrite it as a GADT

--- src/Javascript.hs
+++ src/Javascript.hs
@@ -1,11 +1,14 @@
 {-# language DataKinds #-}
-{-# language KindSignatures #-}
-{-# language GADTs #-}
-{-# language PolyKinds #-}
+{-# language DeriveFunctor #-}
 {-# language ExistentialQuantification #-}
-{-# language TypeOperators #-}
-{-# language TypeFamilies #-}
+{-# language StandaloneDeriving #-}
+{-# language GADTs #-}
+{-# language KindSignatures #-}
+{-# language LambdaCase #-}
+{-# language PolyKinds #-}
 {-# language RankNTypes #-}
+{-# language TypeFamilies #-}
+{-# language TypeOperators #-}
 
 module Javascript 
   ( -- * Types
@@ -16,7 +19,7 @@ module Javascript
     -- * Construction
   , literal
     -- * Interpretation
-  , interpret
+--  , interpret
   ) where
 
 import Data.Kind
@@ -32,12 +35,12 @@ data Value :: Universe -> Type where
   ValueString :: Text -> Value 'String
   ValueArray :: [Value u] -> Value ('Array u)
 
--- data Rec :: (Universe -> Type) -> [Universe] -> Type where
-  -- RecNil :: Rec f '[]
-  -- RecCons :: _ -> Rec f rs -> Rec f (r ': rs)
+--data Rec :: (Universe -> Type) -> [Universe] -> Type where
+--  RecNil :: Rec f '[]
+--  RecCons :: Rec f r -> Rec f rs -> Rec f (r ': rs)
 
 -- data Function :: Type -> [Universe] -> Universe -> Type where
-  -- Function :: STRef s _ -> Function s rs res
+-- Function :: STRef s _ -> Function s rs res
 
 -- When generating code, we ignore the type of the value
 -- since we are just going to produce a monotonically
@@ -51,32 +54,53 @@ type family Bound (t :: Strategy) (u :: Universe) :: Type where
 newtype Binding :: Type -> Strategy -> Universe -> Type where
   Binding :: STRef s (Bound t u) -> Binding s t u
 
--- Are we evaluating or generating code.
+-- Are we evaluating or generating code
 data Strategy = Evaluate | Generate
 
--- TODO: Use GADT syntax for Action. Existential quantification makes
--- H98 data constructor syntax difficult to read.
-data Action s (t :: Strategy) n
-  = forall (u :: Universe). Literal (Value u) (Binding s t u -> n)
-  | Plus (Binding s t 'Number) (Binding s t 'Number) (Binding s t 'Number -> n)
-  | Log (Binding s t 'String) n
-  | forall (u :: Universe). Foreach (Binding s t 'Number) (Binding s t ('Array u)) (Binding s t u -> Free (Action s t) ()) n
-    -- ^ Foreach is an inherently imperative construct. Consequently, it does not
-    -- return anything.
+data Action s (t :: Strategy) n where
+  Literal :: ()
+    => Value u
+    -> (Binding s t u -> n)
+    -> Action s t n
+  Plus :: ()
+    => Binding s t 'Number
+    -> Binding s t 'Number
+    -> (Binding s t 'Number -> n)
+    -> Action s t n
+  Log :: ()
+    => Binding s t 'String
+    -> n
+    -> Action s t n
+  Foreach :: ()
+    => Binding s t 'Number
+    -> Binding s t ('Array u)
+    -> Binding s t u
+    -> Free (Action s t) ()
+    -> n
+    -> Action s t n
+  -- ^ Foreach is an inherently imperative construct. Consequently, it does not
+  --   return anything.
+
+{-
   -- | forall (rs :: [Universe]) (res :: Universe). Declare (Rec (Binding s t) rs -> Free (Action s t) (Binding s t res)) (Function rs res -> n)
     -- ^ Not totally sure if Declare should have the function arrow in its first arg.
   -- | forall (rs :: [Universe]) (res :: Universe). Call (Function rs res) (Rec Value rs) (Value res -> n)
+-}
+
+deriving instance Functor (Action s t)
 
 type JSM s n = Free (Action s n)
 
 -- Create a binding to an literal
 literal :: Value u -> JSM s n (Binding s t u)
-literal value = undefined
-
-interpret :: (forall s t. Free (Action s t) (Binding s t u)) -> Value u
-interpret a = internalInterpret a
+literal = \case
+  ValueNumber i -> liftF (Literal (ValueNumber i) undefined)
+  ValueString s -> liftF (Literal (ValueString s) undefined)
+  ValueArray _ -> error "idk"
+ 
+--interpret :: (forall s t. Free (Action s t) (Binding s t u)) -> Value u
+--interpret a = internalInterpret a
 
 -- Not exported
-internalInterpret :: (forall s t. Free (Action s 'Evaluate) (Binding s t u)) -> Value u
-internalInterpret = _ -- write me
-
+--internalInterpret :: (forall s t. Free (Action s 'Evaluate) (Binding s t u)) -> Value u
+--internalInterpret = _ -- write me
