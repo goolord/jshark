aca58e6559d22f7e52a6bee74eb878ea9d1c4766
chessai <chessai1996@gmail.com>
2019-03-28 07:38:36 -0400

    sort Prelude

--- src/Prelude.hs
+++ src/Prelude.hs
@@ -2,24 +2,24 @@ module Prelude
   ( module P
   ) where
 
-import Data.Eq as P (Eq(..))
-import Data.Ord as P (Ord(..))
-import GHC.Integer as P (Integer)
-import Data.Text as P (Text)
-import Control.Monad.Free as P (Free(..),liftF)
 import Control.Applicative as P (Applicative(..))
 import Control.Monad as P (Monad(..))
+import Control.Monad.Free as P (Free(..),liftF)
 import Control.Monad.ST as P (ST, runST)
 import Data.Bool as P (Bool(..), (&&), (||), otherwise)
-import Data.Function as P (($),id,(&))
-import Data.Semiring as P (Semiring(..), (+),(*), Ring(..), (-))
 import Data.Either as P (Either(..),either)
+import Data.Eq as P (Eq(..))
+import Data.Function as P (($),id,(&))
 import Data.Functor as P (Functor(..))
 import Data.IORef as P
 import Data.Int as P
 import Data.Kind as P (Type,Constraint)
 import Data.Maybe as P (Maybe(..),maybe,fromMaybe,catMaybes)
+import Data.Ord as P (Ord(..))
 import Data.STRef as P
+import Data.Semiring as P (Semiring(..), (+),(*), Ring(..), (-))
+import Data.Text as P (Text)
 import Data.Type.Equality as P 
 import Data.Word as P
 import GHC.Err as P (error,undefined)
+import GHC.Integer as P (Integer)
