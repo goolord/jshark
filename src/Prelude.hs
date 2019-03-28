c58dc84419d0567b4e261dc20079934d737a25b9
chessai <chessai1996@gmail.com>
2019-03-28 07:31:37 -0400

    add Key Monad

--- src/Prelude.hs
+++ src/Prelude.hs
@@ -2,16 +2,24 @@ module Prelude
   ( module P
   ) where
 
-import Data.Functor as P (Functor(..))
-import Control.Monad as P (Monad(..))
+import Data.Eq as P (Eq(..))
+import Data.Ord as P (Ord(..))
+import GHC.Integer as P (Integer)
+import Data.Text as P (Text)
+import Control.Monad.Free as P (Free(..),liftF)
 import Control.Applicative as P (Applicative(..))
-import Data.Kind as P (Type,Constraint)
-import Data.STRef as P
+import Control.Monad as P (Monad(..))
 import Control.Monad.ST as P (ST, runST)
-import Data.Int as P
-import Data.Word as P
-import Data.Bool as P (Bool(..), (&&), (||))
-import Data.Maybe as P (Maybe(..),maybe,fromMaybe,catMaybes)
+import Data.Bool as P (Bool(..), (&&), (||), otherwise)
+import Data.Function as P (($),id,(&))
+import Data.Semiring as P (Semiring(..), (+),(*), Ring(..), (-))
 import Data.Either as P (Either(..),either)
+import Data.Functor as P (Functor(..))
+import Data.IORef as P
+import Data.Int as P
+import Data.Kind as P (Type,Constraint)
+import Data.Maybe as P (Maybe(..),maybe,fromMaybe,catMaybes)
+import Data.STRef as P
+import Data.Type.Equality as P 
+import Data.Word as P
 import GHC.Err as P (error,undefined)
-
