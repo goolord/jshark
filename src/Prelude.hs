0f786eb2ac43bc24418ad068aaa71a0bdc345b88
chessai <chessai1996@gmail.com>
2019-03-28 07:18:41 -0400

    make Action a Functor and rewrite it as a GADT

--- src/Prelude.hs
+++ src/Prelude.hs
@@ -1,5 +1,17 @@
 module Prelude
-  (
+  ( module P
   ) where
 
+import Data.Functor as P (Functor(..))
+import Control.Monad as P (Monad(..))
+import Control.Applicative as P (Applicative(..))
+import Data.Kind as P (Type,Constraint)
+import Data.STRef as P
+import Control.Monad.ST as P (ST, runST)
+import Data.Int as P
+import Data.Word as P
+import Data.Bool as P (Bool(..), (&&), (||))
+import Data.Maybe as P (Maybe(..),maybe,fromMaybe,catMaybes)
+import Data.Either as P (Either(..),either)
+import GHC.Err as P (error,undefined)
 
