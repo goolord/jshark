7e86de4cb4d34268bb55cfbd5b38aed125d426cb
chessai <chessai1996@gmail.com>
2019-03-29 15:37:57 -0400

    remove Identifier, make GenIdentifier safer

--- src/Identifier.hs
+++ src/Identifier.hs
@@ -2,22 +2,17 @@
 {-# language GeneralizedNewtypeDeriving #-}
 
 module Identifier
-  ( Identifier(..)
-  , GenIdentifier(..)
+  ( GenIdentifier
   , genIdentifier
   ) where
 
-import Control.Monad.Trans.State (StateT(..),State)
-import Control.Monad.State.Class (MonadState(..))
+import Control.Monad.Trans.State.Strict
 
-newtype Identifier = Identifier { getIdentifier :: Int64 }
-
-newtype GenIdentifier a = GenIdentifier (State Identifier a)
+newtype GenIdentifier a = GenIdentifier (State Int64 a)
   deriving newtype (Functor,Applicative,Monad)
-  deriving newtype (MonadState Identifier)
 
-genIdentifier :: GenIdentifier Identifier
-genIdentifier = do
+genIdentifier :: GenIdentifier Int64
+genIdentifier = GenIdentifier $ do
   i <- get
-  put $ Identifier (getIdentifier i + 1)
+  put $ (i + 1)
   pure i
