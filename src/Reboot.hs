2155e943260d31af2b61a40c5f160f5a9506d6f3
goolord <zacharyachurchill@gmail.com>
2019-04-05 15:27:14 -0400

    types module

--- src/Reboot.hs
+++ src/Reboot.hs
@@ -1,24 +1,12 @@
 {-# language BangPatterns #-}
-{-# language DataKinds #-}
-{-# language DeriveFunctor #-}
-{-# language DerivingStrategies #-}
-{-# language EmptyCase #-}
-{-# language ExistentialQuantification #-}
-{-# language FlexibleInstances #-}
 {-# language GADTs #-}
-{-# language KindSignatures #-}
 {-# language LambdaCase #-}
 {-# language OverloadedStrings #-}
-{-# language PolyKinds #-}
 {-# language RankNTypes #-}
-{-# language ScopedTypeVariables #-}
-{-# language StandaloneDeriving #-}
-{-# language TypeFamilies #-}
 {-# language TypeInType #-}
 {-# language TypeOperators #-}
 
 {-# options_ghc -fno-warn-unused-top-binds #-}
-{-# options_ghc -fno-warn-orphans #-}
 
 module Reboot
   ( Expr(..)
@@ -39,6 +27,7 @@ module Reboot
 -- https://www.reddit.com/r/haskell/comments/85een6/sharing_from_phoas_multiple_interpreters_from_free/dvxhlba
 
 -- import qualified Data.Sequence as Seq
+import Types
 import Control.Applicative
 import Control.Monad.ST
 import Data.Coerce
@@ -53,79 +42,9 @@ import Unsafe.Coerce (unsafeCoerce)
 import Topaz.Types
 import Topaz.Rec ((<:))
 import qualified Data.Text as T
-import qualified GHC.Exts as Exts
 import qualified Language.JavaScript.AST as GP
 import qualified Language.JavaScript.Pretty as GP
 import qualified Text.PrettyPrint.Leijen as PP
-
-data Universe
-  = Number
-  | String
-  | Unit
-  | Element
-  | List Universe
-  | Effectful Universe
-  | Function Universe Universe
-
-data Value :: Universe -> Type where
-  ValueNumber :: Double -> Value 'Number
-  ValueString :: Text -> Value 'String
-  ValueEffect :: (forall f. Effect f u) -> Value ('Effectful u)
-  ValueFunction :: (Value u -> Value v) -> Value ('Function u v)
-  ValueUnit :: Value 'Unit
-
-data Effect :: (Universe -> Type) -> Universe -> Type where
-  Host :: (f 'String -> Effect f u) -> Effect f u
-  Log :: Expr f u -> Effect f u' -> Effect f u'
-  LookupId :: Expr f 'String -> (f 'Element -> Effect f u) -> Effect f u
-  LookupSelector :: Expr f 'String -> (f ('List 'Element) -> Effect f u) -> Effect f u
-  Lift :: Expr f u -> Effect f u
-  FFI :: String -> Rec (Expr f) (u' ': us) -> Effect f u
-
-data Expr :: (Universe -> Type) -> Universe -> Type where
-  Literal :: Value u -> Expr f u
-  Concat :: Expr f 'String -> Expr f 'String -> Expr f 'String
-  Plus :: Expr f 'Number -> Expr f 'Number -> Expr f 'Number
-  Times :: Expr f 'Number -> Expr f 'Number -> Expr f 'Number
-  Minus :: Expr f 'Number -> Expr f 'Number -> Expr f 'Number
-  Abs :: Expr f 'Number -> Expr f 'Number
-  Sign :: Expr f 'Number -> Expr f 'Number
-  Negate :: Expr f 'Number -> Expr f 'Number
-  Let :: Expr f u -> (f u -> Expr f v) -> Expr f v
-  Lambda :: (f u -> Expr f v) -> Expr f ('Function u v)
-  Apply :: Expr f ('Function u v) -> Expr f u -> Expr f v
-  Show :: Expr f u -> Expr f 'String
-  Var :: f u -> Expr f u
-
-data ExprF :: (Type -> Type -> Type) -> (Universe -> Type) -> Universe -> Type where
-  LiteralF :: Value u -> ExprF g f u
-  PlusF :: ExprF g f 'Number -> ExprF g f 'Number -> ExprF g f 'Number
-  LetF :: ExprF g f u -> g (f u) (ExprF g f v) -> ExprF g f v
-  LambdaF :: g (f u) (ExprF g f v) -> ExprF g f ('Function u v)
-  ApplyF :: ExprF g f ('Function u v) -> ExprF g f u -> ExprF g f v
-  VarF :: f u -> ExprF g f u
-
-data Statement :: (Universe -> Type) -> Universe -> Type where
-  SLiteral :: Value u -> Statement f u
-  SFFI :: Text -> Statement f u
-
--- newtype Expression :: (Universe -> Type) -> Universe -> Type where
---   Expression :: ExprArrow (->) f u -> Expression f u
-
--- Op :: Operation _ u -> ExprArrow g f u
--- data Operation :: () -> Universe -> Type
-  -- Plus :: Operation f 'Number -> Operation f 'Number -> Operation f 'Number
-
-instance forall (f :: (Universe -> Type)) u. (u ~ 'String) => Exts.IsString (Expr f u) where
-  fromString = Literal . ValueString . Exts.fromString
-
-instance forall (f :: (Universe -> Type)) u. (u ~ 'Number) => Num (Expr f u) where
-  (+) = Plus
-  (*) = Times
-  (-) = Minus
-  abs = Abs
-  signum = Sign
-  fromInteger = number . fromInteger
 
 unNumber :: Value 'Number -> Double
 unNumber (ValueNumber d) = d
@@ -199,24 +118,6 @@ evaluate e0 = go e0 where
     Lambda g -> ValueFunction (go . g)
     Concat x y -> ValueString (unString (go x) <> unString (go y))
     _ -> undefined -- just to get rid of errors for now
-
-data Optimization 
-  = ConstantFolding
-  | UnusedBindings
-
-data Computation = Computation GP.Expr (Seq GP.VarStmt)
-newtype EffComputation = EffComputation (Seq (Either GP.VarStmt GP.Expr))
-
-instance PP.Pretty EffComputation where
-  pretty (EffComputation x) = foldr1 (PP.<$$>) (fmap PP.pretty x)
-
-instance PP.Pretty Computation where
-  pretty (Computation ex vars) = 
-    foldr1 (PP.<$$>) ( fmap PP.pretty vars |> PP.pretty ex)
-
-instance (PP.Pretty a, PP.Pretty b) => PP.Pretty (Either a b) where
-  pretty (Left a) = PP.pretty a
-  pretty (Right a) = PP.pretty a
 
 printComputation :: Computation -> IO ()
 printComputation (computation) = do
